8568e7931252ce6dfc98d54966fb79f33f406d37
julen98 <julenramon792@gmail.com>
2022-02-17 22:24:50 +0100

    t4 ej2 v2

--- src/main/java/me/julen/t4/ej2/Main.java
+++ src/main/java/me/julen/t4/ej2/Main.java
@@ -3,6 +3,7 @@ package me.julen.t4.ej2;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.rmi.ConnectIOException;
 import java.util.Scanner;
 
 public class Main {
@@ -13,6 +14,7 @@ public class Main {
             Scanner teclado = new Scanner(System.in);
             String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
             String tipo;
+            
 
             while (!end) {
                 System.out.println("Introduce la url: ");
@@ -24,11 +26,20 @@ public class Main {
                     end = false;
                     URL url = new URL(input);
                     URLConnection connection = url.openConnection();
-                    tipo = connection.getContentType();
+                    tipo = connection.getContentType().toString();
                     System.out.println("Tipo: " + tipo);
-
-                    if(tipo.matches("/^image.*$/")) {
-                        System.out.println("Peso: " + connection.getContentLength());
+                    String[] partes = tipo.split("/");
+                    String subtipo = partes[0];
+                    
+                    if (subtipo.equals("image") || subtipo.equals("video")) {
+                        double peso = connection.getContentLengthLong() / 1000000;
+                        System.out.println("Peso: " + peso + " MB");
+                    } else if (subtipo.equals("text")) {
+                        System.out.println("Codificación: " + connection.getContentEncoding());
+                    } else {
+                        double peso = connection.getContentLengthLong() / 1000000;
+                        System.out.println("Peso: " + peso + " MB");
+                        System.out.println("Fecha de modificación: " + connection.getLastModified());
                     }
                 } else {
                     System.out.println("La URL introducida es incorrecta.");
